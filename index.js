3a76582c05d9befaf025c7c18a0b44c0724f9a67
Pedro Medina <48919013+TyrWinDev@users.noreply.github.com>
2021-10-14 16:28:28 -0400

    Made Changes to the API fetch.

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const autoCompleteConfig = {
     const imgSRC = game.thumb === "N/A" ? " " : game.thumb;
     return `
     <img src="${imgSRC}" />
-    ${game.external}
-    ${game.cheapest}
+    ${game.title}
+    ${game.salePrice}
   `;
   },
   inputValue(game) {
-    return game.external;
+    return game.title;
   },
   async fetchData(searchTerm) {
     const response = await axios.get(
-      "https://www.cheapshark.com/api/1.0/games?",
+      "https://www.cheapshark.com/api/1.0/deals?",
       {
         params: {
           title: searchTerm,
-          limit: 10,
+          pageSize: 10,
         },
       }
     );
@@ -46,12 +46,7 @@ let leftGame;
 let rightGame;
 const onGameSelect = async (game, summaryElement, side) => {
   const response = await axios.get(
-    "https://www.cheapshark.com/api/1.0/games?",
-    {
-      params: {
-        id: game.gameID,
-      },
-    }
+    `https://www.cheapshark.com/api/1.0/deals?id=${game.dealID}`
   );
 
   console.log(response.data);
@@ -73,33 +68,47 @@ const runComparison = () => {
 };
 
 const gameTemplate = (gameDetail) => {
+  //To compare pricing and rating you can use
+  // const dealPrice = parseFloat(gameDetail.deals[0].price)
+
+  // for metascore rating it would be the same thing just using a different variable
+  // const metaScoreRating = parseInt(gameDetail.rating.replace(/,/g, '')
+
   return `
     <article class="media">
       <figure class='media-left'>
         <p class='image'>
-          <img src="${gameDetail.info.thumb}" />
+          <img src="${gameDetail.gameInfo.thumb}" />
         </p>
       </figure>
       <div class='media-content'>
         <div class='content'>
-        <h1>${gameDetail.info.title}</h1>
+        <h1>${gameDetail.gameInfo.name}</h1>
         <h5>Game Genre goes here...</h5>
-        <h4>${gameDetail.deals[0].price}</h4>
-        <p>Add to Favorites</p>
         
         <p>Game summary goes here...</p>
 
-        <input type="button" onclick="location.href='https://www.cheapshark.com/redirect?dealID=${gameDetail.deals[0].dealID}';" value=" Get this Deal!" /> 
+        <input type='button' value='Add to Favorites' />
+
         </div>
       </div>
-    </article>
-    <article class="notification is-primary">
-      <p class='title'>Steam or metacritic score goes here...</p>
-      <p class='subtitle'>Rating</p>
-    </article>
+      </article>
+      <article class="notification is-primary">
+        <p class='title'>${gameDetail.gameInfo.salePrice}</p>
+        <p class='subtitle'>Cheapest Price</p>
+
+      </article>
+      <article class="notification is-primary">
+        <p class='title'>${gameDetail.gameInfo.steamRatingPercent} (${gameDetail.gameInfo.steamRatingText}) </p>
+        <p class='subtitle'>Steam Rating</p>
+      </article>
     <article class="notification is-primary">
       <p class='title'>Gameplay Trailer goes here</p>
       <p class='subtitle'>Gameplay</p>
     </article>
   `;
 };
+
+{
+  /* <input type="button" onclick="location.href='https://www.cheapshark.com/redirect?dealID=${gameDetail.dealID}';" value=" Get this Deal!" />  */
+}
